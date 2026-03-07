212b92ee9ad98c13cd75f2558fbf9456297900b9
Alhaji Alie Sesay <alhajialie6@gmail.com>
2026-03-08 00:30:36 +0200

    8 march 2026 - 12:03am

--- root/scripts/script.js
+++ root/scripts/script.js
@@ -57,3 +57,14 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const contactBtn = document.getElementById('contactBtn');
+
+    if (contactBtn) {
+        contactBtn.addEventListener('click', function() {
+            window.location.href = '/root/pages/contact.html';
+        });
+    }
+});
